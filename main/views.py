f82ee28b85f45bef220b38bee0b09edd92102f1a
Gonzalo <gonzalo.silva.goday@fing.edu.uy>
2021-01-14 18:34:12 -0300

    ENVIO MAILS

--- main/views.py
+++ main/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from .models import *
 from django.template import loader
+from carniceria.settings import EMAIL_HOST_USER
+from . import forms
+from django.core.mail import send_mail
 
 def index(request):
+  form = forms.EnvioMail()
+  if request.method == 'POST':
+    form = forms.EnvioMail(request.POST)
+    if form.is_valid():
+      nombre = form.cleaned_data['nombre']
+      apellido = form.cleaned_data['apellido']
+      email = form.cleaned_data['email']
+      mensaje = form.cleaned_data['mensaje']
+      send_mail("Mensaje recibido en laonce.com.uy", "Ha recibido el mensaje de " + nombre + ", " + apellido + ".\n" + mensaje, EMAIL_HOST_USER, ['rafaschol11@gmail.com'])  
+      print("envioo")
+    print("entro al post")
   destacados = Producto.objects.all()[:8]
   context = {
     'destacados': destacados,
+    'form' : form,
   }
   return render(request, 'main/index.html', context)
 
