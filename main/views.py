a55bfa5e60051b9a9d1d251ad82dbb31893b7120
Gonzalo <gonzalo.silva.goday@fing.edu.uy>
2021-01-14 19:39:23 -0300

    envio mails con alertas

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from carniceria.settings import EMAIL_HOST_USER
 from . import forms
 from django.core.mail import send_mail
+from django.contrib import messages
 
 def index(request):
   form = forms.EnvioMail()
@@ -14,9 +15,13 @@ def index(request):
       apellido = form.cleaned_data['apellido']
       email = form.cleaned_data['email']
       mensaje = form.cleaned_data['mensaje']
-      send_mail("Mensaje recibido en laonce.com.uy", "Ha recibido el mensaje de " + nombre + ", " + apellido + ".\n" + mensaje, EMAIL_HOST_USER, ['rafaschol11@gmail.com'])  
-      print("envioo")
-    print("entro al post")
+      try:
+        send_mail("Mensaje recibido en laonce.com.uy", "Ha recibido el mensaje de " + nombre + ", " + apellido + ".\n" + mensaje, EMAIL_HOST_USER, ['gonga.silva1999@gmail.com'])
+        messages.success(request, ' Se ha enviado correctamente el mensaje. ') 
+      except:
+        messages.error(request,' Ha ocurrido un error, verifique los datos,')
+    else:
+      messages.error(request, 'Ha ocurrido un error, verifique los datos ingresados')
   destacados = Producto.objects.all()[:8]
   context = {
     'destacados': destacados,
