71cfa145bbcd620b54e73f76e0eb18b508e085d8
root <root@vps.server.local>
2021-01-22 13:15:09 +0000

    deploy

--- carniceria/settings.py
+++ carniceria/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'wtbv#n5ka=ordg11u(359dc7$*z1^l0gua2a4b#$b(767=7^3&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["31.220.49.42", "carnicerialaonce.com.uy", "www.carnicerialaonce.com.uy"]
 
